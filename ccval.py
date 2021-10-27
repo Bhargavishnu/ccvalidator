7c396edad0a36fccf3cdc5bcf2eb778dfedddd6d
Christian Clauss <cclauss@me.com>
2021-10-27 22:35:20 +0200

    Reformat the code with psf/black

--- ccval.py
+++ ccval.py
@@ -1,42 +1,45 @@
 def hasnumbers(cnum):
     return cnum.isdigit()
+
+
 def check_len(length):
-    if length >= 13 and length <= 16 :
+    if length >= 13 and length <= 16:
         return True
     return False
 
+
 def startwith(cnum):
-    f=int(cnum[0])
-    if f==4 or f==5 or f==6:
+    f = int(cnum[0])
+    if f == 4 or f == 5 or f == 6:
         return True
-    elif f==3:
-        s=int(cnum[1])
-        if s==7 or s==4 or s==5:
+    elif f == 3:
+        s = int(cnum[1])
+        if s == 7 or s == 4 or s == 5:
             return True
         else:
             return False
     return False
 
+
 def luhn_check(cnum):
-    halflen=len(cnum)-2
-    tsum=0
-    for i in range(halflen,-1,-2):
-        n=int(cnum[i])
-        n=n*2
-        if n>9:
-            n=n%10
-            n=n+1
-        cnum=cnum[:i]+str(n)+cnum[i+1:]
-        tsum+=n
-    for i in range(len(cnum)-1,-1,-2):
-        tsum+=int(cnum[i])
-    if tsum%10==0:
+    halflen = len(cnum) - 2
+    tsum = 0
+    for i in range(halflen, -1, -2):
+        n = int(cnum[i])
+        n = n * 2
+        if n > 9:
+            n = n % 10
+            n = n + 1
+        cnum = cnum[:i] + str(n) + cnum[i + 1 :]
+        tsum += n
+    for i in range(len(cnum) - 1, -1, -2):
+        tsum += int(cnum[i])
+    if tsum % 10 == 0:
         return True
     return False
 
-    
-    
-cnumber=str(input("Enter a credit card number"))
+
+cnumber = str(input("Enter a credit card number"))
 if hasnumbers(cnumber):
     if check_len(len(cnumber)):
         if startwith(cnumber):
@@ -52,4 +55,3 @@ else:
     print("Invalid:Contains alphabets or special characters")
 
 print("THANK YOU")
-    
--- test_ccvalidator.py
+++ test_ccvalidator.py
@@ -6,43 +6,51 @@ from ccval import hasnumbers
 
 class TestSum(unittest.TestCase):
     def test_hasnumber(self):
-        data="4563838383838388"
-        result=hasnumbers(data)
-        self.assertEqual(result,True)
-    def test_nothasnumber(self):
-        data="abcdefg"
-        result=hasnumbers(data)
-        self.assertEqual(result,False)
-    def test_startwith4(self):
-        data="45"
-        result=ccval.startwith(data)
-        self.assertEqual(result,True)
-    def test_startwith5(self):
-        data="55"
-        result=ccval.startwith(data)
-        self.assertEqual(result,True)
-    def test_startwith6(self):
-        data="65"
-        result=ccval.startwith(data)
-        self.assertEqual(result,True)
-    def test_startwith37(self):
-        data="3765"
-        result=ccval.startwith(data)
-        self.assertEqual(result,True)
-    def test_startwith34(self):
-        data="3465"
-        result=ccval.startwith(data)
-        self.assertEqual(result,True)
-    def test_startwith35(self):
-        data="3565"
-        result=ccval.startwith(data)
-        self.assertEqual(result,True)
-    def test_luhn_check(self):
-        data="79927398713"
-        result=ccval.luhn_check(data)
-        self.assertEqual(result,True)
+        data = "4563838383838388"
+        result = hasnumbers(data)
+        self.assertEqual(result, True)
 
-        
-if __name__ == '__main__':
+    def test_nothasnumber(self):
+        data = "abcdefg"
+        result = hasnumbers(data)
+        self.assertEqual(result, False)
+
+    def test_startwith4(self):
+        data = "45"
+        result = ccval.startwith(data)
+        self.assertEqual(result, True)
+
+    def test_startwith5(self):
+        data = "55"
+        result = ccval.startwith(data)
+        self.assertEqual(result, True)
+
+    def test_startwith6(self):
+        data = "65"
+        result = ccval.startwith(data)
+        self.assertEqual(result, True)
+
+    def test_startwith37(self):
+        data = "3765"
+        result = ccval.startwith(data)
+        self.assertEqual(result, True)
+
+    def test_startwith34(self):
+        data = "3465"
+        result = ccval.startwith(data)
+        self.assertEqual(result, True)
+
+    def test_startwith35(self):
+        data = "3565"
+        result = ccval.startwith(data)
+        self.assertEqual(result, True)
+
+    def test_luhn_check(self):
+        data = "79927398713"
+        result = ccval.luhn_check(data)
+        self.assertEqual(result, True)
+
+
+if __name__ == "__main__":
     unittest.main()
     print("Completed all test Successfully")
